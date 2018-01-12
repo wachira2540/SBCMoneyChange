bcc619dee308d8e29f09fec2d4ec44316a6680ba
wachira2540 <wachira2540@gmail.com>
2018-01-12 16:06:16 +0700

    Finish 12/Jan/2018

--- app/src/main/java/tae/sbc/ac/th/sbcmoneychange/MainActivity.java
+++ app/src/main/java/tae/sbc/ac/th/sbcmoneychange/MainActivity.java
@@ -3,11 +3,24 @@ package tae.sbc.ac.th.sbcmoneychange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import tae.sbc.ac.th.sbcmoneychange.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment to Activity
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+
+        }
+
+
+    }//Main Method
+}//Main Class
