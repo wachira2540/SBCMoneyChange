8cdb4cb7b066065c9656ab1d500c25b4e5ab6635
wachira2540 <wachira2540@gmail.com>
2018-01-13 11:50:21 +0700

    Create Toolbar Success

--- app/src/main/java/tae/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
+++ app/src/main/java/tae/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package tae.sbc.ac.th.sbcmoneychange.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import tae.sbc.ac.th.sbcmoneychange.MainActivity;
 import tae.sbc.ac.th.sbcmoneychange.R;
 
 /**
@@ -14,6 +16,35 @@ import tae.sbc.ac.th.sbcmoneychange.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Toolbar Controller
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity())
+                .setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle(getString(R.string.th_exchange));
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setSubtitle(getString(R.string.th_sub_exchange));
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }//Main Method
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
